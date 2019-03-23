045033a461e35df3b961dce4ad0434a625ff8a5f
billylvn <sabilifarhan@gmail.com>
2019-03-23 11:11:52 +0700

    hallo dude

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BELAJAR DONG</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div class="kepala">
+		<center>Data Siswa</center>
+	</div>	
+	<table>		
+		<tr class="judul">
+			<td>No</td>
+			<td>Nama</td>
+			<td>NPM</td>
+			<td>Nilai</td>
+		</tr>
+		<tr>
+			<td>1</td>
+			<td class="nama">Juned</td>
+			<td>154</td>
+			<td class="hijau">90</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td class="nama">Roni</td>
+			<td>265</td>
+			<td class="merah">60</td>
+		</tr>
+		<tr>
+			<td>3</td>
+			<td class="nama">Jimmy</td>
+			<td>285</td>
+			<td class="merah">54</td>
+		</tr>
+		<tr>
+			<td>4</td>
+			<td class="nama">Ivan</td>
+			<td>265</td>
+			<td class="hijau">83</td>
+		</tr>	
+	</table>	
+</body>
+</html>
---
+++ profile.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Profil</title>
+</head>
+<body>
+	<center>HALOO<center>
+	<center><a href="index.php">Balik lagi yuk!</a></center>
+</body>
+</html>
